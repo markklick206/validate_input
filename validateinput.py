2264058b16651ed0c5beede895fa7caed48229da
markklick206 <mklick16@my.whitworth.edu>
2018-02-15 14:20:50 -0800

    fixed typos

--- validateinput.py
+++ validateinput.py
@@ -1,7 +1,12 @@
+# mk
+# validate_input
+# Simple validate_input function using regex patterns to detect numeric or string input.
+
+# import the python regular expression package
+import re
+
 def numericValuesValidation(input_numeric):
-    # import the python regular expression package
-    import re
-    # use re.compile for clarity. This is a pattern for any numeric. \d for float and + for as many times up to a decimal point 
+	# use re.compile for clarity. This is a pattern for any numeric. \d for float and + for as many times up to a decimal point 
     # \. then \d+ again for any number of numbers after the decimal point
     pattern = re.compile('\d+(\.\d+)?')
     # re.match function on a compiled regex object will check to see if this input_numeric variable matches the numeric pattern
@@ -11,7 +16,6 @@ def numericValuesValidation(input_numeric):
     return float(input_numeric)
 
 def stringValuesValidation(input_string):
-    import re
     # custom string matching regex
     pattern = re.compile('[a-zA-Z1-9]+')
 	# check to see if input matches your desired string pattern
